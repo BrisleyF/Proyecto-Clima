08f66def23c143ca380cbdd8c5254b3e2f83e9e1
BrisleyF <fajardobrisley@gmail.com>
2022-05-16 16:29:17 -0400

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,77 @@
+const container = document.querySelector(".container");
+const resultado = document.querySelector("#resultado");
+const formulario = document.querySelector("#formulario");
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+})
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    console.log('Buscando el clima...');
+
+    // validar 
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        // hubo un error
+        mostrarError("Ambos campos son obligatorios");
+        
+        return;
+    }
+
+    // consultariamos la API
+    consultarAPI(ciudad, pais);
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector(".bg-red-100");
+
+    if(!alerta) {
+        // crear una alerta
+        const alerta = document.createElement("div");
+
+        alerta.classList.add(
+        "bg-red-100",
+        "border-red-400",
+        "text-red-700",
+        "px-4",
+        "py-3",
+        "rounded",
+        "max-w-md",
+        "mx-auto",
+        "mt-6",
+        "text-center"
+        );
+
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
+}
+
+function consultarAPI(ciudad, pais) {
+
+
+    const appId = 'ad93500af745c17a9d1dcd9a479846bb';
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( datos => {
+            console.log(datos);
+            if(datos.cod === "404") {
+                mostrarError("Ciudad no encontrada");
+            }
+        })
+}
